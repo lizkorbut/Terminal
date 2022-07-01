b70246f043349a0caa035a66aeee8cbfb627f5d8
Liza-Vika <106345945+lizkorbut@users.noreply.github.com>
2022-07-01 15:58:08 +0300

    Terminal home work

---
+++ sript_terminal_1.sh
@@ -0,0 +1,12 @@
+cd qa_1
+mkdir qa_4 qa_5 qa_6
+cd[A[D[A[D
+[A[C[D
+[Ac[C[[C[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[../
+cd qa_1
+mkdir qa_4 qa_5 qa_6 
+cd qa_4
+touch q1.txt q2.txt q3.txt 1.json 2.json
+mkdir qa_7 qa_8 qa_9
+ls -la
+mv qa11/1.json qa11/2.json qa12
